e4c88445cf79272691a8789988a6ae1c009c0fbc
Thamarai <tmanalan710@gmail.com>
2025-06-12 16:46:09 +0530

    first commit

---
+++ Components/Box_Background.tsx
@@ -0,0 +1,7 @@
+export const Box_Background = ()=>{
+    return (
+        <div className="h-screen w-full flex items-center justify-center bg-[linear-gradient(to_right,#ccc_1px,transparent_1px),linear-gradient(to_bottom,#ccc_1px,transparent_1px)] bg-[size:24px_24px]">
+            <button className="text-white">hello</button>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Box_Background} from "@/Components/Box_Background"
+
+const Home = ()=>{
+  return <>
+  <Box_Background/>
+  </>
+}
+export default Home
